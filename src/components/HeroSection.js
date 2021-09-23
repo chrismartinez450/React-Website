23214e3003b4f5c248f17a6a9be9fcd35ff9da3b
chrismartinez450 <christopher.martinez.450@my.csun.edu>
2021-09-23 15:55:23 -0700

    fixed herosection name

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>Christopher</h1>
+      <h1>Christopher Martinez</h1>
       <p>Bachelor's of Science in Computer Science</p>
       <p>Graduation Date: December 2021</p>
     </div>
