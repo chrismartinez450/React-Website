37723d7749dadcda1a3ff7edcfd1ef42bbb4b8f4
chrismartinez450 <christopher.martinez.450@my.csun.edu>
2021-09-23 11:55:42 -0700

    git

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Resume() {
+  return (
+    <>
+      <h1 className='resume'><a href='documents/resume.docx' download>Click to download my Resumé</a></h1>
+    </>
+  )
+}
